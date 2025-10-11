54dc393cebad784546d27e879b3e0d188d842c10
Andrey Derzhavin <derzhavin.andrej@yandex.ru>
2025-10-11 23:57:31 +0300

    Enable -Werror for CI

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -5,5 +5,9 @@ target_compile_features(prot_defaults INTERFACE cxx_std_20)
 # Compile stuff
 target_compile_options(prot_defaults INTERFACE -Wall -Wextra -Wpedantic
                                                -Wold-style-cast -Wvla)
+if(PROT_ENABLE_WERROR)
+  message(STATUS "-Werror enabled, have fun :)")
+  target_compile_options(prot_defaults INTERFACE -Werror)
+endif()
 
 add_library(PROT::defaults ALIAS prot_defaults)
