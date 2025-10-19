c990593bcc5999d6b60fcbb36b6c83501d767eae
Andrey Derzhavin <derzhavin.andrej@yandex.ru>
2025-10-19 18:24:39 +0300

    [cpu_state] Support pretty dump

--- src/cpu_state/cpu_state.cc
+++ src/cpu_state/cpu_state.cc
@@ -9,8 +9,20 @@ void CPUState::dump(std::ostream &ost) const {
   fmt::println(ost, "Icount = {}", icount);
   fmt::println(ost, "PC = {:#x}", pc);
 
-  for (std::size_t i = 0; i < regs.size(); ++i) {
-    fmt::println(ost, "X[{}] = {:#x}", i, regs[i]);
+  static constexpr std::size_t kRegPerRow = 5;
+  static constexpr std::size_t kNumRows =
+      (kNumRegs + kRegPerRow - 1) / kRegPerRow;
+  static_assert(kNumRows * kRegPerRow >= kNumRegs);
+
+  for (auto i : std::views::iota(0U, kNumRows)) {
+    for (auto j : std::views::iota(0U, kRegPerRow)) {
+      const auto effIdx = (i * kRegPerRow) + j;
+      if (effIdx >= regs.size()) {
+        break;
+      }
+      fmt::print(ost, "X[{:02}] = {:#010x} ", effIdx, regs[effIdx]);
+    }
+    ost << "\n";
   }
 
   fmt::println(ost, "---CPU STATE DUMP END---");
