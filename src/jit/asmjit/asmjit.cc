4178a850878d8a3fa94b1b0a3193f9318a68efe3
uslstenn <shurygin0171@gmail.com>
2025-10-24 02:30:25 +0300

    [jit] asmjit backend was fully supported

--- src/jit/asmjit/asmjit.cc
+++ src/jit/asmjit/asmjit.cc
@@ -18,8 +18,7 @@
     cc.mov(rs1, getReg(insn.rs1()));                                           \
     cc.mov(rs2, getReg(insn.rs2()));                                           \
     cc.ASMJIT_OP(rs1, rs2);                                                    \
-    if (insn.rd() != 0)                                                        \
-      cc.mov(getReg(insn.rd()), rs1);                                          \
+    setDst(insn.rd(), rs1);                                                    \
     break;                                                                     \
   }
 
@@ -27,14 +26,25 @@
   case k##OP##I: {                                                             \
     cc.mov(rs1, getReg(insn.rs1()));                                           \
     cc.ASMJIT_OP(rs1, insn.imm());                                             \
-    if (insn.rd() != 0)                                                        \
-      cc.mov(getReg(insn.rd()), rs1);                                          \
+    setDst(insn.rd(), rs1);                                                    \
     break;                                                                     \
   }
 
 #define PROT_ASMJIT_ALU_OP(OP, ASMJIT_OP)                                      \
   PROT_ASMJIT_R_OP(OP, ASMJIT_OP)                                              \
   PROT_ASMJIT_I_OP(OP, ASMJIT_OP)
+
+#define PROT_ASMJIT_I_SHIFT_OP(OP, ASMJIT_OP)                                  \
+  case k##OP##I: {                                                             \
+    cc.mov(rs1, getReg(insn.rs1()));                                           \
+    cc.ASMJIT_OP(rs1, insn.rs2());                                             \
+    setDst(insn.rd(), rs1);                                                    \
+    break;                                                                     \
+  }
+
+#define PROT_ASMJIT_SHIFT_OP(OP, ASMJIT_OP)                                    \
+  PROT_ASMJIT_R_OP(OP, ASMJIT_OP)                                              \
+  PROT_ASMJIT_I_SHIFT_OP(OP, ASMJIT_OP)
 
 #define PROT_ASMJIT_R_CMP_OP(OP, ASMJIT_OP)                                    \
   case k##OP: {                                                                \
@@ -43,8 +53,7 @@
     cc.cmp(rs2, rs1);                                                          \
     cc.ASMJIT_OP(rd);                                                          \
     cc.movzx(rd, rd.r8());                                                     \
-    if (insn.rd() != 0)                                                        \
-      cc.mov(getReg(insn.rd()), rd);                                           \
+    setDst(insn.rd(), rd);                                                     \
     break;                                                                     \
   }
 
@@ -52,43 +61,21 @@
   case k##OP: {                                                                \
     cc.mov(rs1, getReg(insn.rs1()));                                           \
     cc.cmp(rs1, insn.imm());                                                   \
-    cc.ASMJIT_OP(rd);                                                          \
+    cc.ASMJIT_OP(rd.r8());                                                     \
     cc.movzx(rd, rd.r8());                                                     \
-    if (insn.rd() != 0)                                                        \
-      cc.mov(getReg(insn.rd()), rd);                                           \
+    setDst(insn.rd(), rd);                                                     \
     break;                                                                     \
   }
 
-#define PROT_ASMJIT_B_COND_OP(OP, ASMJIT_OP)                                   \
-  case k##OP: {                                                                \
-    asmjit::Label l_end = cc.newLabel();                                       \
-    asmjit::Label lf = cc.newLabel();                                          \
-    cc.mov(rs1, getReg(insn.rs1()));                                           \
-    cc.mov(rs2, getReg(insn.rs2()));                                           \
-    cc.cmp(rs1, rs2);                                                          \
-    cc.ASMJIT_OP(lf);                                                          \
-    cc.mov(pc, getPC());                                                       \
-    cc.add(pc, insn.imm());                                                    \
-    cc.jmp(l_end);                                                             \
-    cc.bind(lf);                                                               \
-    cc.mov(pc, getPC());                                                       \
-    cc.add(pc, sizeof(isa::Word));                                             \
-    cc.bind(l_end);                                                            \
-    cc.mov(getPC(), pc);                                                       \
-    break;                                                                     \
-  }
-
-#define ANOTHER_PROT_ASMJIT_B_COND_OP(OP, COND)                                \
+#define PROT_ASMJIT_B_COND_OP(OP, COND)                                        \
   case k##OP: {                                                                \
     cc.mov(rs1, getReg(insn.rs1()));                                           \
     cc.mov(rs2, getReg(insn.rs2()));                                           \
-    cc.mov(pc, getPC());                                                       \
-    cc.mov(rd, pc);                                                            \
-    cc.add(rd, sizeof(isa::Word));                                             \
-    cc.add(pc, insn.imm());                                                    \
     cc.cmp(rs1, rs2);                                                          \
-    cc.cmov(COND, pc, rd);                                                     \
-    cc.mov(getPC(), pc);                                                       \
+    cc.mov(rs1, isa::kWordSize);                                               \
+    cc.mov(rs2, insn.imm());                                                   \
+    cc.cmov(COND, rs1, rs2);                                                   \
+    cc.add(getPC(), rs1);                                                      \
     break;                                                                     \
   }
 
@@ -112,9 +99,10 @@
     cc.mov(rs1, getReg(insn.rs1()));                                           \
     cc.add(rs1, insn.imm());                                                   \
     asmjit::InvokeNode *invoke = nullptr;                                      \
-    cc.invoke(&invoke, reinterpret_cast<size_t>(loadHelper<DATA_TYPE>),        \
-              asmjit::FuncSignatureT<DATA_TYPE, CPUState &, isa::Addr>());     \
-    invoke->setArg(0, &state);                                                 \
+    cc.invoke(                                                                 \
+        &invoke, reinterpret_cast<size_t>(loadHelper<DATA_TYPE>),              \
+        asmjit::FuncSignature::build<DATA_TYPE, CPUState &, isa::Addr>());     \
+    invoke->setArg(0, state_ptr);                                              \
     invoke->setArg(1, rs1);                                                    \
     invoke->setRet(0, rd);                                                     \
     if (insn.rd() != 0) {                                                      \
@@ -130,14 +118,13 @@
     cc.mov(rs1, getReg(insn.rs1()));                                           \
     cc.add(rs1, insn.imm());                                                   \
     asmjit::InvokeNode *invoke = nullptr;                                      \
-    cc.invoke(&invoke, reinterpret_cast<size_t>(loadHelper<DATA_TYPE>),        \
-              asmjit::FuncSignatureT<DATA_TYPE, CPUState &, isa::Addr>());     \
-    invoke->setArg(0, &state);                                                 \
+    cc.invoke(                                                                 \
+        &invoke, reinterpret_cast<size_t>(loadHelper<DATA_TYPE>),              \
+        asmjit::FuncSignature::build<DATA_TYPE, CPUState &, isa::Addr>());     \
+    invoke->setArg(0, state_ptr);                                              \
     invoke->setArg(1, rs1);                                                    \
     invoke->setRet(0, rd);                                                     \
-    if (insn.rd() != 0)                                                        \
-      cc.mov(getReg(insn.rd()), rd);                                           \
-                                                                               \
+    setDst(insn.rd(), rd);                                                     \
     break;                                                                     \
   }
 
@@ -165,10 +152,9 @@ private:
       return false;
     }
 
-    m_state_ptr = &state;
-    auto holder = translate(state, *bbInfo);
+    auto holder = translate(*bbInfo);
 
-    auto [it, wasNew] = m_cacheTB.try_emplace(pc, holder);
+    auto [it, wasNew] = m_cacheTB.try_emplace(pc, std::move(holder));
     assert(wasNew);
 
     it->second(state);
@@ -176,12 +162,10 @@ private:
     return true;
   }
 
-  [[nodiscard]] JitFunction translate(CPUState &state, const BBInfo &info);
+  [[nodiscard]] JitFunction translate(const BBInfo &info);
 
   asmjit::JitRuntime runtime;
   std::unordered_map<isa::Addr, JitFunction> m_cacheTB;
-  std::uint32_t test_count = 0;
-  CPUState *m_state_ptr{nullptr};
 };
 
 template <typename T> void storeHelper(CPUState &state, isa::Addr addr, T val) {
@@ -194,38 +178,29 @@ template <typename T> T loadHelper(CPUState &state, isa::Addr addr) {
 
 void syscallHelper(CPUState &state) { state.emulateSysCall(); }
 
-JitFunction AsmJit::translate(CPUState &state, const BBInfo &info) {
+JitFunction AsmJit::translate(const BBInfo &info) {
   asmjit::CodeHolder code;
-  // asmjit::FileLogger logger(stdout);
-  // code.setLogger(&logger);
   code.init(runtime.environment());
 
   asmjit::x86::Compiler cc(&code);
+  auto signature = asmjit::FuncSignature::build<void, CPUState *>();
+  auto *func_node = cc.addFunc(signature);
 
-  asmjit::FuncSignature signature =
-      asmjit::FuncSignature::build<void, CPUState *>();
-  asmjit::FuncNode *func_node = cc.addFunc(signature);
+  auto state_ptr = cc.newUIntPtr();
+  func_node->setArg(0, state_ptr);
 
-  auto *state_ptr = &state;
-  // cc.mov(state_ptr, asmjit::x86::dword_ptr((size_t)(&state)));
-  // func_node->setArg(0, state_ptr);
-
-  constexpr isa::Word regs_offset = offsetof(CPUState, regs);
-  constexpr isa::Word pc_offset = offsetof(CPUState, pc);
-
-  auto getReg = [&state_ptr](std::size_t regId) {
-    return asmjit::x86::dword_ptr(reinterpret_cast<size_t>(state_ptr) +
-                                  regs_offset + sizeof(isa::Word) * regId);
-    // так есть segfault
-    // return asmjit::x86::dword_ptr(
-    //     (size_t)(&state.regs[regId])); // так программа завершается корректно
+  auto getReg = [&state_ptr](auto regId) {
+    return asmjit::x86::dword_ptr(state_ptr, offsetof(CPUState, regs) +
+                                                 isa::kWordSize * regId);
   };
 
   auto getPC = [&state_ptr]() {
-    return asmjit::x86::dword_ptr(reinterpret_cast<size_t>(state_ptr) +
-                                  pc_offset); // так есть segfault
-    // return asmjit::x86::dword_ptr(
-    //     (size_t)(&state.pc)); // так программа завершается корректно
+    return asmjit::x86::dword_ptr(state_ptr, offsetof(CPUState, pc));
+  };
+
+  auto setDst = [&cc, getReg](auto dstId, auto dst) {
+    if (dstId != 0)
+      cc.mov(getReg(dstId), dst);
   };
 
   auto pc = cc.newGpd();
@@ -236,36 +211,30 @@ JitFunction AsmJit::translate(CPUState &state, const BBInfo &info) {
   for (const auto &insn : info.insns) {
     switch (insn.opcode()) {
       using enum isa::Opcode;
+      using enum asmjit::x86::CondCode;
       PROT_ASMJIT_ALU_OP(ADD, add)
       PROT_ASMJIT_ALU_OP(AND, and_)
       PROT_ASMJIT_ALU_OP(OR, or_)
       PROT_ASMJIT_ALU_OP(XOR, xor_)
 
-      PROT_ASMJIT_ALU_OP(SLL, shl)
-      PROT_ASMJIT_ALU_OP(SRA, sar)
-      PROT_ASMJIT_ALU_OP(SRL, shr)
+      PROT_ASMJIT_SHIFT_OP(SLL, shl)
+      PROT_ASMJIT_SHIFT_OP(SRA, sar)
+      PROT_ASMJIT_SHIFT_OP(SRL, shr)
 
       PROT_ASMJIT_R_OP(SUB, sub)
 
       PROT_ASMJIT_R_CMP_OP(SLT, setg)
       PROT_ASMJIT_R_CMP_OP(SLTU, seta)
 
-      PROT_ASMJIT_I_CMP_OP(SLTI, setg)
-      PROT_ASMJIT_I_CMP_OP(SLTIU, seta)
+      PROT_ASMJIT_I_CMP_OP(SLTI, setl)
+      PROT_ASMJIT_I_CMP_OP(SLTIU, setb)
 
-      // ANOTHER_PROT_ASMJIT_B_COND_OP(BEQ, asmjit::x86::CondCode::kEqual)
-      // ANOTHER_PROT_ASMJIT_B_COND_OP(BNE, asmjit::x86::CondCode::kNotEqual)
-      // ANOTHER_PROT_ASMJIT_B_COND_OP(BLT, asmjit::x86::CondCode::kSignedLT)
-      // ANOTHER_PROT_ASMJIT_B_COND_OP(BGE, asmjit::x86::CondCode::kSignedGE)
-      // ANOTHER_PROT_ASMJIT_B_COND_OP(BLTU, asmjit::x86::CondCode::kUnsignedLT)
-      // ANOTHER_PROT_ASMJIT_B_COND_OP(BGEU, asmjit::x86::CondCode::kUnsignedGE)
-
-      PROT_ASMJIT_B_COND_OP(BEQ, jne)
-      PROT_ASMJIT_B_COND_OP(BNE, je)
-      PROT_ASMJIT_B_COND_OP(BGE, jl)
-      PROT_ASMJIT_B_COND_OP(BGEU, jb)
-      PROT_ASMJIT_B_COND_OP(BLT, jge)
-      PROT_ASMJIT_B_COND_OP(BLTU, jae)
+      PROT_ASMJIT_B_COND_OP(BEQ,  kEqual)
+      PROT_ASMJIT_B_COND_OP(BNE,  kNotEqual)
+      PROT_ASMJIT_B_COND_OP(BLT,  kSignedLT)
+      PROT_ASMJIT_B_COND_OP(BGE,  kSignedGE)
+      PROT_ASMJIT_B_COND_OP(BLTU, kUnsignedLT)
+      PROT_ASMJIT_B_COND_OP(BGEU, kUnsignedGE)
 
       PROT_ASMJIT_L_OP(LB, prot::isa::Byte)
       PROT_ASMJIT_L_OP(LH, prot::isa::Half)
@@ -281,11 +250,8 @@ JitFunction AsmJit::translate(CPUState &state, const BBInfo &info) {
     // PROT_ASMJIT_J_OP
     case kJAL: {
       cc.mov(rd, getPC());
-      cc.add(rd, sizeof(isa::Word));
-
-      if (insn.rd() != 0)
-        cc.mov(getReg(insn.rd()), rd);
-
+      cc.add(rd, isa::kWordSize);
+      setDst(insn.rd(), rd);
       cc.mov(pc, getPC());
       cc.add(pc, insn.imm());
       cc.mov(getPC(), pc);
@@ -293,7 +259,7 @@ JitFunction AsmJit::translate(CPUState &state, const BBInfo &info) {
     }
     case kJALR: {
       cc.mov(rd, getPC());
-      cc.add(rd, sizeof(isa::Word));
+      cc.add(rd, isa::kWordSize);
 
       cc.mov(pc, getReg(insn.rs1()));
       cc.add(pc, insn.imm());
@@ -309,16 +275,14 @@ JitFunction AsmJit::translate(CPUState &state, const BBInfo &info) {
     // PROT_ASMJIT_U_OP
     case kLUI: {
       cc.mov(rs1, insn.imm());
-      if (insn.rd() != 0)
-        cc.mov(getReg(insn.rd()), rs1);
+      setDst(insn.rd(), rs1);
       break;
     }
 
     case kAUIPC: {
       cc.mov(rs1, getPC());
       cc.add(rs1, insn.imm());
-      if (insn.rd() != 0)
-        cc.mov(getReg(insn.rd()), rs1);
+      setDst(insn.rd(), rs1);
       break;
     }
 
@@ -328,13 +292,6 @@ JitFunction AsmJit::translate(CPUState &state, const BBInfo &info) {
                 asmjit::FuncSignature::build<void, CPUState &>());
       invoke->setArg(0, state_ptr);
       break;
-
-      // auto helper = reinterpret_cast<void (*)(CPUState *)>(&syscallHelper);
-      // cc.push(state_ptr);
-      // cc.mov(asmjit::x86::rax, reinterpret_cast<uintptr_t>(helper));
-      // cc.call(asmjit::x86::rax);
-      // cc.pop(state_ptr);
-      // break;
     }
 
     case kFENCE:
@@ -351,7 +308,7 @@ JitFunction AsmJit::translate(CPUState &state, const BBInfo &info) {
 
     if (!isa::changesPC(insn.opcode())) {
       cc.mov(pc, getPC());
-      cc.add(pc, sizeof(isa::Word));
+      cc.add(pc, isa::kWordSize);
       cc.mov(getPC(), pc);
     }
   }
@@ -361,9 +318,8 @@ JitFunction AsmJit::translate(CPUState &state, const BBInfo &info) {
 
   JitFunction func;
   auto err = runtime.add(&func, &code);
-  if (err) {
+  if (err)
     throw std::runtime_error{"Failed to generate code"};
-  }
 
   return func;
 }
