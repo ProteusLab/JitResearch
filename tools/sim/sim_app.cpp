a3edb22255dd221f8bc051d1f92c3bb6f2815be2
VasiliyMatr <vasiliymatrenin@gmail.com>
2026-02-23 17:03:37 +0300

    [psim] Add support for basic metrics collection
    Use perf-cpp for perf counters collection

--- tools/sim/sim_app.cpp
+++ tools/sim/sim_app.cpp
@@ -1,10 +1,22 @@
 #include <CLI/CLI.hpp>
 
-#include <chrono>
+#include <cstdint>
 #include <filesystem>
+#include <fstream>
+#include <ios>
+#include <ranges>
+#include <string>
+#include <utility>
+#include <vector>
 
+#include <fmt/base.h>
 #include <fmt/core.h>
+#include <fmt/format.h>
 #include <fmt/ostream.h>
+#include <fmt/ranges.h>
+
+#include <perfcpp/counter_result.h>
+#include <perfcpp/event_counter.h>
 
 #include "prot/elf_loader.hh"
 #include "prot/hart.hh"
@@ -12,12 +24,63 @@
 #include "prot/jit/factory.hh"
 #include "prot/memory.hh"
 
-int main(int argc, const char *argv[]) try {
+namespace {
 
+struct RunConfig {
+  const std::filesystem::path &elfPath;
+  prot::isa::Addr stackTop = 0x7fffffff;
+  const std::string &jitBackend;
+  const std::vector<std::string> &perfEvents;
+  bool dumpHart = false;
+};
+
+struct RunResult {
+  uint32_t status = 0;
+  uint64_t guestIc = 0;
+  perf::CounterResult perfRes;
+};
+
+RunResult run(const RunConfig &cfg) {
+  auto hart = [&] {
+    prot::ElfLoader loader{cfg.elfPath};
+
+    std::unique_ptr<prot::ExecEngine> engine =
+        !cfg.jitBackend.empty()
+            ? prot::engine::JitFactory::createEngine(cfg.jitBackend)
+            : std::make_unique<prot::engine::Interpreter>();
+
+    prot::Hart hart{prot::memory::makePlain(4ULL << 30U), std::move(engine)};
+    hart.load(loader);
+    hart.setSP(cfg.stackTop);
+
+    return hart;
+  }();
+
+  auto eventCounter = perf::EventCounter{};
+  eventCounter.add(cfg.perfEvents);
+
+  eventCounter.start();
+  hart.run();
+  eventCounter.stop();
+
+  if (cfg.dumpHart) {
+    hart.dump(std::cout);
+  }
+
+  return RunResult{.status = hart.getExitCode(),
+                   .guestIc = hart.getIcount(),
+                   .perfRes = eventCounter.result()};
+}
+
+} // namespace
+
+int main(int argc, const char *argv[]) try {
   std::filesystem::path elfPath;
   constexpr prot::isa::Addr kDefaultStack = 0x7fffffff;
   prot::isa::Addr stackTop{};
   std::string jitBackend{};
+  std::filesystem::path statsCsvPath;
+  bool dumpHart = false;
 
   {
     CLI::App app{"App for JIT research from ProteusLab team"};
@@ -33,33 +96,93 @@ int main(int argc, const char *argv[]) try {
     app.add_option("--jit", jitBackend, "Use JIT & set backend")
         ->check(CLI::IsMember(prot::engine::JitFactory::backends()));
 
+    app.add_option(
+        "--stats", statsCsvPath,
+        "Path to CSV stats file. Enables stats collection regime if set");
+
+    app.add_flag("--dump", dumpHart, "Dump hart values on each run");
+
     CLI11_PARSE(app, argc, argv);
   }
 
-  auto hart = [&] {
-    prot::ElfLoader loader{elfPath};
+  bool collectStats = !statsCsvPath.empty();
 
-    std::unique_ptr<prot::ExecEngine> engine =
-        !jitBackend.empty() ? prot::engine::JitFactory::createEngine(jitBackend)
-                            : std::make_unique<prot::engine::Interpreter>();
+  // Group related counters together
+  // clang-format off
+  std::vector<std::vector<std::string>> counters {
+    {
+      "seconds",
+      "instructions",
+      "cycles",
+      "branches",
+      "branch-misses",
+    },
+    {
+      "L1-dcache-loads",
+      "L1-dcache-load-misses",
+      "L1-icache-loads",
+      "L1-icache-load-misses",
+      "iTLB-load-misses",
+      "dTLB-load-misses",
+    },
+  };
+  // clang-format on
 
-    prot::Hart hart{prot::memory::makePlain(4ULL << 30U), std::move(engine)};
-    hart.load(loader);
-    hart.setSP(stackTop);
+  std::ofstream statsCsv{};
+  if (collectStats) {
+    std::filesystem::create_directories(statsCsvPath.parent_path());
 
-    return hart;
-  }();
+    bool addHeader = !std::filesystem::exists(statsCsvPath);
+    statsCsv.open(statsCsvPath, std::ios_base::app | std::ios_base::out);
 
-  auto start = std::chrono::high_resolution_clock::now();
-  hart.run();
-  auto end = std::chrono::high_resolution_clock::now();
-  hart.dump(std::cout);
-  std::chrono::duration<double> duration = end - start;
-  fmt::println("icount: {}", hart.getIcount());
-  fmt::println("time: {}s", duration.count());
-  fmt::println("threshold: {}", 0);
-  fmt::println("mips: {}", hart.getIcount() / (duration.count() * 1000000));
-  return hart.getExitCode();
+    if (addHeader) {
+      fmt::println(statsCsv, "{}", fmt::join(counters | std::views::join, ","));
+    }
+  }
+
+  auto runNum = 0;
+  for (const auto &runCounters :
+       counters | std::views::take(collectStats ? 0xffU : 1U)) {
+    fmt::println("Run #{}", runNum++);
+
+    auto res = run({
+        .elfPath = elfPath,
+        .stackTop = stackTop,
+        .jitBackend = jitBackend,
+        .perfEvents = runCounters,
+        .dumpHart = dumpHart,
+    });
+
+    auto seconds = res.perfRes["seconds"];
+    auto cycles = res.perfRes["cycles"];
+    auto hostInstrs = res.perfRes["instructions"];
+
+    if (seconds) {
+      fmt::println("  time: {:.2f}s", *seconds);
+    }
+
+    if (seconds && cycles && hostInstrs) {
+      auto guestIPS = res.guestIc / *seconds;
+      fmt::println("  MIPS: {:.2f}", guestIPS / 1'000'000);
+    }
+
+    if (collectStats) {
+      fmt::print(statsCsv, "{}",
+                 fmt::join(res.perfRes | std::views::values, ","));
+    }
+
+    fmt::println("  status = {}", res.status);
+    if (res.status != 0) {
+      fmt::println("Run failed, aborting");
+      return res.status;
+    }
+  }
+
+  if (collectStats) {
+    statsCsv << "\n";
+  }
+
+  return 0;
 } catch (const std::exception &ex) {
   fmt::println(std::cerr, "Caught an exception of type {}, message: {}",
                typeid(ex).name(), ex.what());
